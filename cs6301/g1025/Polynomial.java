bd5b4a1d879f1c0fe1555fac5393d21942c8d9e8
evamy <antriksh.don@gmail.com>
2017-09-07 12:01:11 -0500

    Polynomial class added * imports two polynomials from two files given in arguments * can add two polynomials * can evaluate a polynomial with a given value of x * working on multiplication of two polynomials

---
+++ cs6301/g1025/Polynomial.java
@@ -0,0 +1,124 @@
+package cs6301.g1025;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Polynomial {
+
+    static class Tuple implements Comparable<Tuple>{
+        final Integer coefficient;
+        final Integer exponent;
+        Tuple(Integer coefficient, Integer exponent){
+            this.coefficient = coefficient;
+            this.exponent = exponent;
+        }
+        public String toString(){
+            return "("+this.coefficient + ", "+this.exponent+")";
+        }
+        public int compareTo(Tuple other){
+            if (this.exponent < other.exponent) {
+                return -1;
+            } else if (this.exponent > other.exponent) {
+                return 1;
+            } else return 0;
+        }
+    }
+
+    static<T> T next(Iterator<T> it) {
+        return (it.hasNext() ? it.next() : null);
+    }
+
+    public Polynomial(){ }
+
+    public static List<Tuple> readPolynomial(Scanner in){
+
+        int lines = in.nextInt();
+
+        List<Tuple> poly = new LinkedList<>();
+        for(int i=0;i<lines;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            poly.add(new Polynomial.Tuple(a, b));
+        }
+        return poly;
+    }
+
+    public void add(List<Tuple> poly1, List<Tuple> poly2, List<Tuple> outPoly){
+        Iterator<Tuple> it1 = poly1.iterator();
+        Iterator<Tuple> it2 = poly2.iterator();
+
+        Tuple item1 = next(it1);
+        Tuple item2 = next(it2);
+
+        while(item1!=null && item2!=null){
+            if(item1.compareTo(item2) < 0){
+                outPoly.add(item1);
+                item1 = next(it1);
+            }
+            else if(item1.compareTo(item2) > 0){
+                outPoly.add(item2);
+                item2 = next(it2);
+            }
+            else{
+                Integer coeff = item1.coefficient + item2.coefficient;
+                if(coeff>0) outPoly.add(new Tuple(coeff, item1.exponent));
+                item1 = next(it1);
+                item2 = next(it2);
+            }
+        }
+        while (item1 != null) {
+            outPoly.add(item1);
+            item1 = next(it1);
+        }
+        while (item2 != null) {
+            outPoly.add(item2);
+            item2 = next(it2);
+        }
+    }
+
+    public void multiply(List<Tuple> poly1, List<Tuple> poly2, List<Tuple> outPoly){
+        return;
+    }
+
+    public double evaluate(List<Tuple> poly1, Integer x){
+
+        double sum = 0;
+        for(Tuple item:poly1){
+            sum += (item.coefficient * Math.pow(x, item.exponent));
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        int evens = 0;
+        Scanner in1, in2;
+        if (args.length > 0) {
+            File inputFile = new File(args[0]);
+            in1 = new Scanner(inputFile);
+            inputFile = new File(args[1]);
+            in2 = new Scanner(inputFile);
+        } else {
+            throw new FileNotFoundException("Input requires two files for two polynomials");
+        }
+
+        List<Tuple> poly1 = readPolynomial(in1);
+        List<Tuple> poly2 = readPolynomial(in2);
+        System.out.println(poly1);
+        System.out.println(poly2);
+
+        List<Tuple> outPoly = new LinkedList<>();
+        Polynomial poly = new Polynomial();
+        poly.add(poly1, poly2, outPoly);
+        System.out.println(outPoly);
+
+//        outPoly = new LinkedList<>();
+//        poly.multiply(poly1, poly2, outPoly);
+
+        System.out.println(poly.evaluate(outPoly, 5));
+    }
+}
--- cs6301/g1025/ShuntingYard.java
+++ cs6301/g1025/ShuntingYard.java
@@ -14,12 +14,10 @@ import java.util.Stack;
 public class ShuntingYard {
     private Stack<Character> stack = new Stack<Character>();
     private Queue<Character> q = new LinkedList<Character>();
-    ;
     public String prefix = "";
     public String infix;
 
-    public ShuntingYard() {
-    }
+    public ShuntingYard() {}
 
     public String toString() {
         return prefix;
@@ -171,22 +169,135 @@ public class ShuntingYard {
         return prefix;
     }
 
+    /** Items Required
+     * Returning items required to perform the operation using the input operator
+     *
+     * @param op: input operator
+     * @return Integer 2/1
+     * @throws IllegalArgumentException
+     */
+    Integer itemsRequired(Character op){
+        switch(op){
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+            case '^':
+                return 2;
+            case '!':
+                return 1;
+            default:
+                throw new IllegalArgumentException("Operator not found.");
+        }
+    }
+
+    /** Evaluate Operator
+     * Performs operator input on two Integers
+     *
+     * @param op input operator
+     * @param first first Integer popped
+     * @param second second Integer popped
+     * @return Integer after performing operation
+     * @throws IllegalArgumentException
+     */
+    Integer evalOperator(Character op, Integer first, Integer second){
+        switch(op){
+            case '+': return first+second;
+            case '-': return first-second;
+            case '*': return first*second;
+            case '/': return first/second;
+            case '^': return (int) Math.pow(first, second);
+            default:
+                throw new IllegalArgumentException("Operator not found.");
+        }
+    }
+
+    /**
+     * Calculating factorial
+     * @param a input integer
+     * @return Integer value of factorial
+     */
+    Integer factorial(Integer a){
+        Integer fact=1;
+        for(int i=2;i<a;i++){
+            fact = fact*i;
+        }
+        return fact;
+    }
+
+    /**
+     * Evaluates operators that require only single Integer
+     *
+     * @param op Operator
+     * @param first Integer
+     * @return Integer value after performing operation
+     * @throws IllegalArgumentException
+     */
+    Integer evalOperator(Character op, Integer first){
+        switch(op){
+            case '!': return factorial(first);
+            default:
+                throw new IllegalArgumentException("Operator not found.");
+        }
+    }
+
+    /**
+     * Evaluates postfix expressions using stack data structure
+     *
+     * @param postfix String postfix expression input
+     * @return Integer value
+     */
+    public Integer evaluate(String postfix){
+        Stack<Integer> stack = new Stack<Integer>();
+
+        for(int i=0;i < postfix.length();i++){
+            Character token = postfix.charAt(i);
+            if (token == ' ')
+                continue;
+            if (token >= '0' && token <= '9') {
+                Integer num = Character.getNumericValue(token);
+                stack.push(num);
+            }
+            else if(isOperator(token)){
+                if(itemsRequired(token)==2){
+                    Integer item1 = stack.pop();
+                    Integer item2 = stack.pop();
+                    stack.push(evalOperator(token, item1, item2));
+                }
+                else{
+                    Integer item = stack.pop();
+                    stack.push(evalOperator(token, item));
+                }
+            }
+        }
+
+        return stack.pop();
+    }
+
     public static void main(String[] args) {
         ShuntingYard s = new ShuntingYard();
         System.out.println("Infix: 3 * 4 * 5 * 6");
-        System.out.println("Postfix: " + s.shuntingYard("3 * 4 * 5 * 6"));
+        String postfix = s.shuntingYard("3 * 4 * 5 * 6");
+        System.out.println("Postfix: " + postfix);
+        System.out.println("Evaluation: " + s.evaluate(postfix));
 
         s = new ShuntingYard();
         System.out.println("Infix: 3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3");
-        System.out.println("Postfix: " + s.shuntingYard("3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3"));
+        postfix = s.shuntingYard("3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3");
+        System.out.println("Postfix: " + postfix);
+        System.out.println("Evaluation: " + s.evaluate(postfix));
 
         s = new ShuntingYard();
         System.out.println("Infix: 3 + 4 * 2 * ( 5! ) + 2 / ( 1 - 5 ) ^ 2 ^ 3");
-        System.out.println("Postfix: " + s.shuntingYard("3 + 4 * 2 * ( 5! ) + 2 / ( 1 - 5 ) ^ 2 ^ 3"));
+        postfix = s.shuntingYard("3 + 4 * 2 * ( 5! ) + 2 / ( 1 - 5 ) ^ 2 ^ 3");
+        System.out.println("Postfix: " + postfix);
+        System.out.println("Evaluation: " + s.evaluate(postfix));
 
         s = new ShuntingYard();
         System.out.println("Infix: 3 * 5^2 - 6^4 + ( 9! ) + 3!");
-        System.out.println("Postfix: " + s.shuntingYard("3 * 5^2 - 6^4 + ( 9! ) + 3!"));
+        postfix = s.shuntingYard("3 * 5^2 - 6^4 + ( 9! ) + 3!");
+        System.out.println("Postfix: " + postfix);
+        System.out.println("Evaluation: " + s.evaluate(postfix));
     }
 
 }
