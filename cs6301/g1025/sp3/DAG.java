527de1903e2a1013b77083e22ad59be9e90175c8
pswaroopk <swaroopkpydisetty@gmail.com>
2017-09-12 13:27:08 -0500

    comments

--- cs6301/g1025/sp3/DAG.java
+++ cs6301/g1025/sp3/DAG.java
@@ -8,121 +8,35 @@ import java.util.ArrayList;
 
 import cs6301.g00.Graph;
 
-//public class DAG extends GraphAlgorithm<DAG.DAGVertex> {
 public class DAG {
-//	int time = 0;
-//	
-//	public DAG(Graph g) {
-//		super(g);
-//		node = new DAGVertex[g.size()];
-//		// Create array for storing vertex properties
-//		for (Graph.Vertex u : g) {
-//			node[u.getName()] = new DAGVertex(u);
-//		}
-//	}
-//	
-//	static class DAGVertex {
-//		boolean seen;
-//		int dis;//discovered time
-//		int fin;//finish time
-//		Graph.Vertex parent;
-//		
-//		DAGVertex(Graph.Vertex u) {
-//			seen = false;
-//			dis = 0;
-//			fin = 0;
-//			parent = null;
-//		}
-//	}
-//	
-//
-//	
-//	void dfs(){
-//		time = 0;
-//		for(Graph.Vertex v : g){
-//			if(!seen(v)){
-//				dfsVisit(v);
-//			}
-//		}
-//
-//	}
-//	
-//	void dfsVisit(Graph.Vertex u){
-//		visit(u);
-//		discover(u);
-//		for(Graph.Edge e : u){
-//			Graph.Vertex v = e.otherEnd(u);
-//			if(!seen(v)){
-//				parent(u,v);
-//				visit(v);
-//				dfsVisit(v);
-//			}
-//		}
-//		finish(u);
-//		visit(u);
-//	}
-//	
-//	Graph.Vertex getParent(Graph.Vertex u) {
-//		return getVertex(u).parent;
-//	}
-//
-//	boolean seen(Graph.Vertex u) {
-//		return getVertex(u).seen;
-//	}
-//	
-//	// Visit a node v from u
-//	void visit(Graph.Vertex v) {
-//		DAGVertex dv = getVertex(v);
-//		dv.seen = true;
-//		
-//	}
-//	
-//	void parent(Graph.Vertex u, Graph.Vertex v){
-//		DAGVertex dv = getVertex(v);
-//		dv.parent = u;
-//	}
-//	void discover(Graph.Vertex v){
-//		DAGVertex dv = getVertex(v);
-//		dv.dis = ++time;
-//		
-//	}
-//	
-//	void finish(Graph.Vertex v){
-//		DAGVertex dv = getVertex(v);
-//		dv.fin = ++time;
-//	}
 	
+	/**
+	 * @param d DFS object after dfs is done.
+	 *calculates back edges. can be reused to calculate the list of back edges in bridge - cut problem
+	 *optimal: the loop can be stopped whenever we find a back edge. 
+	 */
 	boolean hasBackEdges(DFS d){
 		
 		List<Graph.Edge> lst = new ArrayList<Graph.Edge>();
-		
-//		for(Graph.Vertex u : g){
-//			getVertex(u).seen = false;
-//		}
+
 		for(Graph.Vertex u : d.g){
 			
 			for(Graph.Edge e : u){
 				Graph.Vertex v = e.otherEnd(u);
-//				int vDis = d.getVertex(v).dis;
-//				int uDis = d.getVertex(u).dis;
-//				int uFin = d.getVertex(u).fin;
-//				int vFin = d.getVertex(v).fin;
-//				if(vDis <= uDis && uDis < uFin && uFin <= vFin) 
-//					lst.add(e);
+				//(v.discovery <= u.discovery < u.finish <= v.finish), then e is a back edge
 				if(d.getVertex(v).dis <= d.getVertex(u).dis && d.getVertex(u).dis < d.getVertex(u).fin
 						&& d.getVertex(u).fin <= d.getVertex(v).fin){
 					lst.add(e);
+					//return false;
 				}
 			}
-			//getVertex(u).seen = true;
-			
 		}
-		
+		//return true;
 		return lst.size() != 0;
 	}
-	boolean isDAG(Graph g1) {
+	boolean isDAG(Graph g) {
 		
-		DFS d = new DFS(g1, null);
+		DFS d = new DFS(g, null);
 		d.dfs();
 		return hasBackEdges(d);
 		
@@ -140,12 +54,8 @@ public class DAG {
 		}
 		
 		Graph graph = Graph.readGraph(scanner, true);
-		DAG d = new DAG();
-//		d.dfs();
-		System.out.println(d.isDAG(graph));
-		
-		
-	
+		DAG dag = new DAG();
+		System.out.println(dag.isDAG(graph));
 		
 	}
 
