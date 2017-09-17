41c5a5baa2b11ec3adb9d7a97a8048bc1b846d3c
evamy <antriksh.don@gmail.com>
2017-09-17 09:49:38 -0500

    testing Level 1 and 2 functions

--- cs6301/g1025/LP1L1.java
+++ cs6301/g1025/LP1L1.java
@@ -9,11 +9,11 @@ import cs6301.g00.Timer;
 
 public class LP1L1 {
     public static void main(String[] args) {
-        Num x = new Num("123456");
-        Num y = new Num("444");
+        Num x = new Num("1239086231487532401960927341097456");
+        Num y = new Num("100");
         
-        BigInteger a = new BigInteger("123456");
-        //BigInteger b = new BigInteger("9876543219876543219876543219876543219876543210");
+        BigInteger a = new BigInteger("1239086231487532401960927341097456");
+        BigInteger b = new BigInteger("100");
         
         
         Timer timer = new Timer();
@@ -24,12 +24,12 @@ public class LP1L1 {
         System.out.println();
         
         timer.start();
-        BigInteger c = a.pow(4444444);
+        BigInteger c = a.pow(100);
         timer.end();
         System.out.println("BigInteger: " + timer);
         
-        //System.out.println(c);
-        //System.out.println(z);
+        System.out.println(c);
+        System.out.println(z);
         //System.out.println(c.toString().compareTo(z.toString()));
     }
 }
