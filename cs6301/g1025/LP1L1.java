aa0474550b235960a247e6b12c684318e920013e
pswaroopk <swaroopkpydisetty@gmail.com>
2017-09-17 02:44:20 -0500

    timer - efficiency check

--- cs6301/g1025/LP1L1.java
+++ cs6301/g1025/LP1L1.java
@@ -5,16 +5,31 @@ package cs6301.g1025;
 
 import java.math.BigInteger;
 
+import cs6301.g00.Timer;
+
 public class LP1L1 {
     public static void main(String[] args) {
         Num x = new Num("123456");
-        Num y = new Num("44444");
-        Num z = Num.power(x, y );
-        //BigInteger a = new BigInteger("1234567891234567891234567891234567891234567890");
+        Num y = new Num("444");
+        
+        BigInteger a = new BigInteger("123456");
         //BigInteger b = new BigInteger("9876543219876543219876543219876543219876543210");
-        //BigInteger c = a.pow(11);
+        
+        
+        Timer timer = new Timer();
+        timer.start();
+        Num z = Num.power(x, y );
+        timer.end();
+        System.out.println("Num class: "+ timer);
+        System.out.println();
+        
+        timer.start();
+        BigInteger c = a.pow(4444444);
+        timer.end();
+        System.out.println("BigInteger: " + timer);
+        
         //System.out.println(c);
-        System.out.println(z);
+        //System.out.println(z);
         //System.out.println(c.toString().compareTo(z.toString()));
     }
 }
