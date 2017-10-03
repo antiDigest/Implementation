6ae7786b6d20129daefdbe3f36e49661c944e902
pswaroopk <swaroopkpydisetty@gmail.com>
2017-10-03 14:29:56 -0500

    Implemented Q4, modified Modified Graph.java as required

--- cs6301/README.md
+++ cs6301/README.md
@@ -3,7 +3,7 @@
 #Using an IDE:
 Place the extracted folder (cs6301\g1025\..) in a src folder of an IDE (eg. eclipse) and stock files in cs6301\g00\
 
-##Using cmd:
+#Using cmd:
 a. Extract the files into cs6301
 b. cd to g1025
 c. run javac *.java
@@ -11,20 +11,20 @@ d. cd to src
 e. run java cs6301.g1025.<Class> to test the questions
 
 
-Question1: Sorting an array using k-way merge
+##Question1: Sorting an array using k-way merge
 
-Question2: Huffman Coding algorithm.
+##Question2: Huffman Coding algorithm.
 
-Question3: Perfect powers:
+##Question3: Perfect powers:
 
-Question4: Prim's algorithm - 1
+##Question4: Prim's algorithm - 1
 
-Question5: Implementation of binary heap and heap sort
+##Question5: Implementation of binary heap and heap sort
 
-Question6: Prim's algorithm - 2
+##Question6: Prim's algorithm - 2
 
-Question7: Implementation Kruskal's algorithm for MST
+##Question7: Implementation Kruskal's algorithm for MST
 
-Question8: RT analysis of the following two algorithms:
+##Question8: RT analysis of the following two algorithms:
     (a) Use Java's priority queue to keep track of the k largest elements seen
     (b) Use your priority queue implementation (5) by replace() instead of delete+add
--- cs6301/g1025/PrimMST.java
+++ cs6301/g1025/PrimMST.java
@@ -6,23 +6,85 @@ package cs6301.g1025;
 import java.util.Scanner;
 
 import cs6301.g00.Graph;
+import cs6301.g00.Graph.Edge;
+import cs6301.g00.Graph.Vertex;
+import cs6301.g00.GraphAlgorithm;
 import cs6301.g00.Timer;
 
-import java.lang.Comparable;
 import java.io.FileNotFoundException;
 import java.io.File;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
-public class PrimMST {
+public class PrimMST extends GraphAlgorithm<PrimMST.PrimVertex> {
+
+
 	static final int Infinity = Integer.MAX_VALUE;
 
 	public PrimMST(Graph g) {
+		super(g);
+		node = new PrimVertex[g.size()];
+		// Create array for storing vertex properties
+		for (Vertex u : g) {
+			node[u.getName()] = new PrimVertex(u);
+		}
+
+		initialize(g);
+	}
+	
+	public void initialize(Graph g){
+		for (Vertex u : g) {
+			prim(u).seen = false;
+			prim(u).parent = null;
+		}
 	}
 
-	public int prim1(Graph.Vertex s) {
+	/**
+	 * Wrapper to store component numbers
+	 */
+	static class PrimVertex {
+		boolean seen;
+		Graph.Vertex parent;
+
+		PrimVertex(Graph.Vertex u) {
+			seen = false;
+			parent = null;
+		}
+	}
+
+	public PrimVertex prim(Graph.Vertex u) {
+		return Graph.Vertex.getVertex(node, u);
+	}
+
+	// SP6.Q4: Prim's algorithm using PriorityQueue<Edge>:
+	public int prim1(Vertex src) {
 		int wmst = 0;
-
-		// SP6.Q4: Prim's algorithm using PriorityQueue<Edge>:
-
+		PriorityQueue<Graph.Edge> pq = new PriorityQueue<Graph.Edge>(new Comparator<Edge>() {
+			@Override
+			public int compare(Edge e1, Edge e2) {
+				return (e1.weight < e2.weight ? -1 : (e1.weight == e2.weight ? 0 : 1));
+			}
+		});
+		for (Edge e : src) {
+			pq.add(e);
+		}
+		// Graph.Vertex u = src;
+		prim(src).seen = true;
+		prim(src).parent = null;
+		while (!pq.isEmpty()) {
+			Edge e = pq.poll();
+			Vertex u = e.from;// from edge
+			Vertex v = e.to;// to edge
+			if (prim(u).seen && prim(v).seen)
+				continue;// if both seen continue
+			prim(v).seen = true;
+			prim(v).parent = u;
+			wmst += e.weight;// only now add this edge weight to wmst
+			for (Edge edge : v) {
+				if (!prim(edge.otherEnd(v)).seen)
+					pq.add(edge);
+			}
+		}
 		return wmst;
 	}
 
@@ -49,8 +111,17 @@ public class PrimMST {
 
 		Timer timer = new Timer();
 		PrimMST mst = new PrimMST(g);
-		int wmst = mst.prim2(s);
+		timer.start();
+		int wmst = mst.prim1(s);
 		timer.end();
-		System.out.println(wmst);
+		System.out.println("Weight of MST with prim1 algorithm:" + wmst);
+		System.out.println(timer);
+		mst.initialize(g);
+		
+		timer.start();
+		wmst = mst.prim2(s);
+		timer.end();
+		System.out.println("Weight of MST with prim2 algorithm:" + wmst);
+		System.out.println(timer);
 	}
 }
