f023155b281f9da6cd8cdfc5415f0695113cb575
saikumarsuvanam <saikumar.kumar08@gmail.com>
2017-10-07 10:34:46 -0500

    Overrided move for indexheap-working

--- cs6301/g1025/IndexedHeap.java
+++ cs6301/g1025/IndexedHeap.java
@@ -4,13 +4,27 @@ package cs6301.g1025;
 import java.util.Comparator;
 
 public class IndexedHeap<T extends Index> extends BinaryHeap<T> {
-    /** Build a priority queue with a given array q */
-    public IndexedHeap(T[] q, Comparator<T> comp, int n) {
-	super(q, comp, n);
-    }
 
-    /** restore heap order property after the priority of x has decreased */
-    public void decreaseKey(T x) {
-	percolateUp(x.getIndex());
-    }
+	/** Build a priority queue with a given array q */
+	public IndexedHeap(T[] q, Comparator<T> comp, int n) {
+		super(q, comp, n);
+	}
+
+	/**
+	 * restore heap order property after the priority of x has decreased
+	 * 
+	 * @throws PQException
+	 */
+	public void decreaseKey(T x) throws PQException {
+
+		percolateUp(x.getIndex());
+
+	}
+
+	void move(Object[] pq, int i, T x) {
+		x.putIndex(i);
+		pq[i] = x;
+
+	}
+
 }
