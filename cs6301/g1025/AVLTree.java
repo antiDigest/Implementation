c661f5c7d95d664a38e404ba7dc656c55b1c07d6
saikumarsuvanam <saikumar.kumar08@gmail.com>
2017-10-21 19:41:46 -0500

    modified update and rotations and utility methods

--- cs6301/g1025/AVLTree.java
+++ cs6301/g1025/AVLTree.java
@@ -7,254 +7,262 @@ package cs6301.g1025;
 
 import java.util.Scanner;
 
-import static java.lang.Math.abs;
+
+import java.util.Scanner;
 
 public class AVLTree<T extends Comparable<? super T>> extends BST<T> {
-    static class Entry<T> extends BST.Entry<T> {
-        int height;
+	static class Entry<T> extends BST.Entry<T> {
+		int height;
 
-        Entry(T x, Entry<T> left, Entry<T> right) {
-            super(x, left, right);
-            height = 0;
-        }
+		Entry(T x, Entry<T> left, Entry<T> right) {
+			super(x, left, right);
+			height = 0;
+		}
 
-        Entry() {
-            this.element = null;
-        }
+		void setHeight(int h) {
+			this.height = h;
+		}
 
-        Entry<T> getLeft() {
-            return (Entry<T>) this.left;
-        }
+		int getHeight() {
+			return this.height;
+		}
 
-        Entry<T> getRight() {
-            return (Entry<T>) this.right;
-        }
+		Entry<T> getLeft() {
+			return (Entry<T>) this.left;
+		}
 
-        int getLeftHeight() {
-            if (this.left == null)
-                return -1;
-            return this.getLeft().height;
-        }
+		Entry<T> getRight() {
+			return (Entry<T>) this.right;
+		}
 
-        int getRightHeight() {
-            if (this.right == null)
-                return -1;
-            return this.getRight().height;
-        }
+		@Override
+		public String toString() {
+			return this.element + "";
+		}
 
-        void setLeftHeight(int height) {
-            if (this.left == null)
-                return;
-            else if (height > -1)
-                this.getLeft().height = height;
-        }
+	}
 
-        void setRightHeight(int height) {
-            if (this.right == null)
-                return;
-            else if (height > -1)
-                this.getRight().height = height;
-        }
+	AVLTree() {
+		super();
+	}
 
-        void setHeight() {
-            this.height = 1 + Integer.max(this.getLeftHeight(), this.getRightHeight());
-        }
+	/**
+	 * Remove x from tree. Return x if found, otherwise return null
+	 *
+	 * @param x
+	 *            value to remove
+	 * @return x, if found, otherwise null
+	 */
+	public T remove(T x) {
+		T result = super.remove(x);
+		if (result == null)
+			return result;
+		update();
 
-        @Override
-        public String toString() {
-            return this.element + "";
-        }
-    }
+		return result;
+	}
 
-    AVLTree() {
-        root = null;
-        size = 0;
-    }
+	/**
+	 * Add x to tree. If tree contains a node with same key, replace element by
+	 * x. Returns true if x is a new element added to tree.
+	 * 
+	 * @param x
+	 *            T type, value to add
+	 * @return true if added/replaced, false otherwise
+	 */
+	public boolean add(T x) {
+		if (root == null) {
+			root = new Entry<T>(x, null, null);
+			size = 1;
+			return true;
+		}
+		Entry<T> t = (Entry<T>) find(x);
+		boolean res = addHelper(x, t, new Entry<T>(x, null, null));
 
-    void relax(Entry<T> t, T x) {
-        if (t.element.compareTo(x) == 0) {
-            t.element = x; //Replace
-        } else if (t.element.compareTo(x) > 0) {
-            t.left = new Entry<T>(x, null, null);
-            size++;
-        } else {
-            t.right = new Entry<T>(x, null, null);
-            size++;
-        }
-    }
+		if (res) {
+			stack.push(t);
+			update();
+		}
+		return res;
 
-    /**
-     * Add x to tree.
-     * If tree contains a node with same key, replace element by x.
-     * Returns true if x is a new element added to tree.
-     *
-     * @param x T type, value to add
-     * @return true if added/replaced, false otherwise
-     */
-    public boolean add(T x) {
-        if (root == null) {
-            root = new Entry<T>(x, null, null);
-            size = 1;
-            return true;
-        }
-        Entry<T> t = (Entry<T>) find(x);
-        relax(t, x);
-        return update();
-//        return true;
-    }
+	}
 
-    /**
-     * Remove x from tree.
-     * Return x if found, otherwise return null
-     *
-     * @param x value to remove
-     * @return x, if found, otherwise null
-     */
-    public T remove(T x) {
-        T result = super.remove(x);
-        update(); // TODO : check casting here
-        return result;
-    }
+	/**
+	 * set the height of the entry
+	 * 
+	 * @param t
+	 *            Entry<T> type
+	 * @return void
+	 */
+	public void setHeight(Entry<T> t) {
+		t.height = Math.max(height(t.getLeft()), height(t.getRight())) + 1;
 
-    /**
-     * ROTATIONS
-     */
+	}
 
-    Entry<T> right(Entry<T> P, Entry<T> Q) {
-        Entry<T> Pright = P.getRight();
-        P.right = Q;
-        Q.left = Pright;
-        return P;
-    }
+	/**
+	 * After rotation, link the stack peek(parent) with the correct child
+	 * 
+	 * @param head,child
+	 *            Entry<T> type
+	 * @return void
+	 */
+	void linkChild(Entry<T> head, Entry<T> child) {
+		Entry<T> parent = (Entry<T>) stack.peek();
+		if (parent != null) {
+			if (parent.left != null && head.element.compareTo(parent.left.element) == 0) {
+				parent.left = child;
+			} else if (parent.right != null && head.element.compareTo(parent.right.element) == 0) {
+				parent.right = child;
 
-    Entry<T> left(Entry<T> P, Entry<T> Q) {
-        Entry<T> Qleft = Q.getLeft();
-        Q.left = P;
-        P.right = Qleft;
-        return Q;
-    }
+			}
+		}
 
-    Entry<T> leftRight(Entry<T> P, Entry<T> R, Entry<T> Q) {
-        Entry<T> Qright = Q.getRight();
-        Entry<T> Qleft = Q.getLeft();
-        Q.left = R;
-        Q.right = P;
-        P.left = Qright;
-        R.right = Qleft;
-        return Q;
-    }
+	}
 
-    Entry<T> rightLeft(Entry<T> P, Entry<T> R, Entry<T> Q) {
-        Entry<T> Qright = Q.getRight();
-        Entry<T> Qleft = Q.getLeft();
-        Q.right = R;
-        Q.left = P;
-        P.right = Qleft;
-        R.left = Qright;
-        return Q;
-    }
+	/**
+	 * ROTATIONS
+	 */
+	Entry<T> rotateRight(Entry<T> head) {
+		Entry<T> headL = head.getLeft();
+		Entry<T> headLR = headL.getRight();
+		headL.right = head;
+		head.left = headLR;
+		setHeight(head);
+		setHeight(headL);
+		linkChild(head, headL);
+		return headL;
+	}
 
-    /**
-     * UPDATE CALL FOR ROTATIONS
-     */
+	Entry<T> rotateLeft(Entry<T> head) {
+		Entry<T> headR = head.getRight();
+		Entry<T> headRL = headR.getLeft();
+		headR.left = head;
+		head.right = headRL;
+		setHeight(head);
+		setHeight(headR);
+		linkChild(head, headR);
+		return headR;
+	}
 
-    boolean update() {
-        this.root = update((Entry<T>) this.root);
-        return isValid();
-    }
+	/**
+	 * UPDATE CALL FOR ROTATIONS
+	 */
+	private void update() {
 
-    Entry<T> update(Entry<T> u) {
-        if (u == null) return u;
+		Entry<T> t = null;
+		if (stack.peek() == null) {
+			return;
+		}
+		while (true) {
+			t = (Entry<T>) stack.pop();
+            setHeight(t);
+			int balance = getHeightDiff(t);
+			if (balance > 1) {
+				int l = getHeightDiff(t.getLeft());
+				// LL
+				if (l >= 0) {
+					t = rotateRight(t);
+				}
+				// LR
+				if (l < 0) {
+					t.left = rotateLeft(t.getLeft());
+					t = rotateRight(t);
+				}
+			}
 
-        u.left = u.left == null ? null : update(u.getLeft());
-        u.right = u.right == null ? null : update(u.getRight());
+			if (balance < -1) {
+				int r = getHeightDiff(t.getRight());
+				// RR
+				if (r <= 0) {
+					t = rotateLeft(t);
+				}
+				// RL
+				if (r > 0) {
+					t.right = rotateRight(t.getRight());
+					t = rotateLeft(t);
+				}
 
-        int lh = u.getLeftHeight();
-        int rh = u.getRightHeight();
-        boolean flag = false;
+			}
 
-        if (abs(lh - rh) > 1) {
-            if (lh > rh && u.getLeft().getLeftHeight() > u.getLeft().getRightHeight()) {
-                u = right(u.getLeft(), u);
-                flag = true;
-            } else if (rh > lh && u.getRight().getRightHeight() > u.getRight().getLeftHeight()) {
-                u = left(u, u.getRight());
-                flag = true;
-            } else if (lh > rh && u.getLeft().getRightHeight() > u.getLeft().getLeftHeight()) {
-                if(u.getLeft().getRight() != null) {
-                    u = leftRight(u, u.getLeft(), u.getLeft().getRight());
-                    flag = true;
-                }
-            } else if (rh > lh && u.getRight().getLeftHeight() > u.getRight().getRightHeight()) {
-                if(u.getRight().getLeft() != null) {
-                    u = rightLeft(u, u.getRight(), u.getRight().getLeft());
-                    flag = true;
-                }
-            }
-            if(flag){
-                u.left = u.left == null ? null : update(u.getLeft());
-                u.right = u.right == null ? null : update(u.getRight());
-                u.setHeight();
-            }
-        }
+			if (stack.peek() == null) {
+				root = t;
+				break;
+			}
 
-        u.setHeight();
-        return u;
-    }
+		}
 
-    public static void main(String[] args) {
-        AVLTree<Integer> t = new AVLTree<>();
-        Scanner in = new Scanner(System.in);
-        while (in.hasNext()) {
-            int x = in.nextInt();
-            if (x > 0) {
-                System.out.print("Add " + x + " : ");
-                System.out.println(t.add(x));
-                t.printTree();
-            } else if (x < 0) {
-                System.out.print("Remove " + x + " : ");
-                t.remove(-x);
-                t.printTree();
-            } else {
-                Comparable[] arr = t.toArray();
-                System.out.print("Final: ");
-                for (int i = 0; i < t.size; i++) {
-                    System.out.print(arr[i] + " ");
-                }
-                System.out.println();
-                return;
-            }
-        }
-    }
+	}
 
-    /**
-     * CHECKING VALIDITY
-     */
+	/**
+	 * retuns the height of an entry in the tree
+	 * 
+	 * @param t
+	 *            Entry<T> type
+	 * @return int
+	 */
+	int height(Entry<T> t) {
+		if (t == null)
+			return 0;
 
-    boolean isValid() {
-        return isValid((Entry<T>) this.root);
-    }
+		return t.getHeight();
+	}
 
-    boolean isValid(Entry<T> node) {
-        if (node == null) return true;
-        int rh = node.getRightHeight();
-        int lh = node.getLeftHeight();
-        if (node.getLeft() == null && node.getRight() == null) return true;
-        if (abs(lh - rh) <= 1 && node.height == (1 + Integer.max(lh, rh))) {
-            if (node.getLeft() == null && node.getRight().element.compareTo(node.element) > 0)
-                return isValid(node.getRight());
-            else if (node.getRight() == null && node.getLeft().element.compareTo(node.element) < 0)
-                return isValid(node.getLeft());
-            else if (node.getLeft().element.compareTo(node.element) < 0
-                    && node.getRight().element.compareTo(node.element) > 0)
-                return isValid(node.getLeft()) && isValid(node.getRight());
-        }
-        System.out.println("INVALID AVL AT: " + node + " :: " + node.height + "->" +
-                node.getRight() + "[" + node.getRightHeight() + "]::" + node.getLeft() + "[" + node.getRightHeight() + "]");
-        return false;
-    }
+	/**
+	 * retuns the difference in the height of left child and right child
+	 * 
+	 * @param t
+	 *            Entry<T> type
+	 * @return int
+	 */
+
+	int getHeightDiff(Entry<T> t) {
+		if (t == null)
+			return 0;
+
+		return height(t.getLeft()) - height(t.getRight());
+	}
+
+	public static void main(String[] args) {
+		AVLTree<Integer> t = new AVLTree<>();
+		Scanner in = new Scanner(System.in);
+		while (in.hasNext()) {
+			int x = in.nextInt();
+			if (x > 0) {
+				System.out.print("Add " + x + " : ");
+				t.add(x);
+				t.printTree();
+
+			} else if (x < 0) {
+				System.out.print("Remove " + x + " : ");
+				t.remove(-x);
+				t.printTree();
+
+			} else {
+				Comparable[] arr = t.toArray();
+				System.out.print("Final: ");
+				for (int i = 0; i < t.size; i++) {
+					System.out.print(arr[i] + " ");
+				}
+				System.out.println();
+
+				return;
+			}
+		}
+	}
 
 }
+
+/*
+ * Sample input: 1 3 5 7 9 2 4 6 8 10 -3 -6 -3 0
+ * 
+ * Output: Add 1 : [1] 1 Add 3 : [2] 1 3 Add 5 : [3] 1 3 5 Add 7 : [4] 1 3 5 7
+ * Add 9 : [5] 1 3 5 7 9 Add 2 : [6] 1 2 3 5 7 9 Add 4 : [7] 1 2 3 4 5 7 9 Add 6
+ * : [8] 1 2 3 4 5 6 7 9 Add 8 : [9] 1 2 3 4 5 6 7 8 9 Add 10 : [10] 1 2 3 4 5 6
+ * 7 8 9 10 Remove -3 : [9] 1 2 4 5 6 7 8 9 10 Remove -6 : [8] 1 2 4 5 7 8 9 10
+ * Remove -3 : [8] 1 2 4 5 7 8 9 10 Final: 1 2 4 5 7 8 9 10
+ * 
+ */
+
 
 
 /**
