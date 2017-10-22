aa29d0b73f04cfdfc741d02617cf42d0f8c79c67
saikumarsuvanam <saikumar.kumar08@gmail.com>
2017-10-22 13:24:21 -0500

    have to create a sseperate driver program for this

--- cs6301/g1025/SplayTree.java
+++ cs6301/g1025/SplayTree.java
@@ -1,270 +1,291 @@
 package cs6301.g1025;
 
+
 import java.util.Scanner;
 
-import static java.lang.Math.abs;
+
+
 
 public class SplayTree<T extends Comparable<? super T>> extends BST<T> {
-    private SplayTree() {
-        super();
-    }
+	SplayTree() {
+		super();
+	}
 
+	/**
+	 * UTILITY FUNCTIONS
+	 */
 
-    /**
-     * UTILITY FUNCTIONS
-     */
+	/**
+	 * Is x contained in tree?
+	 *
+	 * @param x
+	 *            value to find
+	 * @return true if found, else fasle
+	 */
+	public boolean contains(T x) {
+		Entry<T> t = find(x);
+		boolean result = (t == null) && (t.element.compareTo(x) == 0);
+		if (result)
+			splay(t);
+		return result;
+	}
 
-    /**
-     * Is x contained in tree?
-     *
-     * @param x value to find
-     * @return true if found, else fasle
-     */
-    public boolean contains(T x) {
-        Entry<T> t = find(x);
-        boolean result = (t == null) && (t.element.compareTo(x) == 0);
-        if (result) splay(t);
-        return result;
-    }
+	/**
+	 * Add x to tree,and splay at x. If tree contains a node with same key,
+	 * replace element by x,and will do splay at x. Returns true if x is a new
+	 * element added to tree.
+	 *
+	 * @param x
+	 *            T type, value to add
+	 * @return true if added/replaced, false otherwise
+	 */
+	public boolean add(T x) {
+		Entry<T> obj = new Entry<T>(x, null, null);
+		if (root == null) {
+			return createRoot(obj);
+		}
+		Entry<T> t = find(x);
+		boolean res = addHelper(x, t, obj);
+		//if successfull
+        if (res) {
+			stack.push(t);
+            splay(obj);
+		} 
+        else {
+			splay(t);
+		}
+		return res;
 
-    /**
-     * Add x to tree.
-     * If tree contains a node with same key, replace element by x.
-     * Returns true if x is a new element added to tree.
-     *
-     * @param x T type, value to add
-     * @return true if added/replaced, false otherwise
-     */
-    public boolean add(T x) {
-        boolean add = super.add(x);
-        if (add) {
-            Entry<T> t = find(x);
-            splay(t);
-        }
-        return add;
-    }
+	}
 
-    /**
-     * Is there an element that is equal to x in the tree?
-     * Element in tree that is equal to x is returned, null otherwise.
-     *
-     * @param x T type, value to find
-     * @return x, if present, else null
-     */
-    public T get(T x) {
-        Entry<T> t = find(x);
-        T result = ((t == null) && (t.element.compareTo(x) == 0)) ? (T) t.element : null;
-        if (result != null)
-            splay(t);
-        return result;
-    }
+	/**
+	 * Is there an element that is equal to x in the tree? Element in tree that
+	 * is equal to x is returned, null otherwise.
+	 *
+	 * @param x
+	 *            T type, value to find
+	 * @return x, if present, else null
+	 */
+	public T get(T x) {
+		Entry<T> t = find(x);
+		T result = ((t == null) && (t.element.compareTo(x) == 0)) ? (T) t.element : null;
+		if (result != null)
+			splay(t);
+		return result;
+	}
 
-    /**
-     * EXTRA UTILITY
-     */
+	/**
+	 * ROTATIONS
+	 */
+	private Entry<T> rightRotate(Entry<T> x) {
 
-    /**
-     * Returns minimum from the BST
-     * @return min value (T)
-     */
-    public T min() {
-        T min = super.min();
-        Entry<T> t = find(min);
-        splay(t);
-        return min;
-    }
+		Entry<T> y = x.getLeft();
+		x.left = y.getRight();
+		y.right = x;
+		linkChild(x, y);
+		return y;
 
-    /**
-     * Returns maximum from the BST
-     * @return maximum value (T)
-     */
-    public T max() {
-        T max = super.min();
-        Entry<T> t = find(max);
-        splay(t);
-        return max;
-    }
+	}
 
-    /**
-     * Remove x from tree.
-     * Return x if found, otherwise return null
-     *
-     * @param x value to remove
-     * @return x, if found, otherwise null
-     */
-    public T remove(T x) {
-        T result = super.remove(x);
-        Entry<T> t = find(x);
-        splay(t);
-        return result;
-    }
+	private Entry<T> leftRotate(Entry<T> x) {
+		Entry<T> y = x.getRight();
+		x.right = y.getLeft();
+		y.left = x;
+		linkChild(x, y);
+		return y;
+	}
 
-    /**
-     * Brings the node t to the root !
-     * @param t BST.Entry type, node to be brought to root
-     */
-    private void splay(BST.Entry<T> t) {
-        this.root = splay(this.root, t);
-    }
+	/**
+	 * EXTRA UTILITY
+	 */
 
-    private Entry<T> splay(Entry<T> u, Entry<T> t){
-        if (u == null || u.isLeaf()) return u;
+	/**
+	 * Returns minimum from the BST
+	 * 
+	 * @return min value (T)
+	 */
+	public T min() {
+		T min = super.min();
+		if(min!=null){
+		splay(stack.pop());}
+		return min;
+	}
 
-        u.left = splay(u.getLeft(), t);
-        u.right = splay(u.getRight(), t);
+	/**
+	 * Returns maximum from the BST
+	 * 
+	 * @return maximum value (T)
+	 */
+	public T max() {
+		T max = super.min();
+		if(max!=null){
+		splay(stack.pop());}
+		return max;
+	}
 
-        if (u.getLeft() == t) {
-            u = right(t, u);
-        } else if (u.getRight() == t) {
-            u = left(u, t);
-        } else {
-            if (u.getLeft() != null) {
-                Entry<T> k = u.getLeft();
-                if(k.getLeft() != null && k.getLeft().element.compareTo(t.element) == 0) {
-                    //LL
-                    u = rightRight(u, k, k.getLeft());
-                }
-                else if (k.getRight() != null && k.getRight().element.compareTo(t.element) == 0){
-                    //LR
-                    u = leftRight(u, k, k.getRight());
-                }
-            } else if (u.getRight() != null ) {
-                Entry<T> k = u.getRight();
-                if(k.getLeft() != null && k.getLeft().element.compareTo(t.element) == 0) {
-                    //RL
-                    u = rightLeft(u, k, k.getLeft());
-                }
-                else if (k.getRight() != null && k.getRight().element.compareTo(t.element) == 0){
-                    //LL
-                    u = leftLeft(u, k, k.getRight());
-                }
-            }
-        }
+	/**
+	 * Remove x from tree. Return x if found, otherwise return null
+	 *
+	 * @param x
+	 *            value to remove
+	 * @return x, if found, otherwise null
+	 */
+	public T remove(T x) {
+		if (root == null)
+			return null;
+		Entry<T> t = find(x);
+		T result=super.removehelp(t , x);
+		if(result==null){
+		  splay(t);
+		}
+		else{
+			if(!stack.isEmpty()&&stack.peek()!=null)
+			   splay(stack.pop());
+		}
+		return result;
+	}
 
-        return u;
-    }
+	/**
+	 * After rotation, link the stack peek(parent) with the correct child
+	 * 
+	 * @param head,child
+	 *            Entry<T> type
+	 * @return void
+	 */
+	private void linkChild(Entry<T> head, Entry<T> top) {
+		if (!stack.isEmpty()) {
+			Entry<T> parent = (Entry<T>) stack.peek();
+			if (parent != null) {
+				if (parent.left != null && head.element.compareTo(parent.left.element) == 0) {
+					parent.left = top;
+				} else if (parent.right != null && head.element.compareTo(parent.right.element) == 0) {
+					parent.right = top;
 
-    /**
-     * After rotation, link the stack peek(parent) with the correct child
-     *
-     * @param head Entry<T> type
-     * @param child Entry<T> type
-     * @return void
-     */
-    private void linkChild(Entry<T> head, Entry<T> child) {
-        Entry<T> parent = (Entry<T>) stack.peek();
-        if (parent != null) {
-            if (parent.left != null && head.element.compareTo(parent.left.element) == 0) {
-                parent.left = child;
-            } else if (parent.right != null && head.element.compareTo(parent.right.element) == 0) {
-                parent.right = child;
-            }
-        }
-    }
+				}
+			}
+		}
+	}
 
-    /**
-     * ROTATIONS
-     */
+	/**
+	 * compares the two entries and returns boolean
+	 * 
+	 * @param t1,t2
+	 *            Entry<T> type
+	 * @return boolean
+	 */
+	private boolean compare(Entry<T> t1, Entry<T> t2) {
 
-    private Entry<T> right(Entry<T> P, Entry<T> Q) {
-        Entry<T> Pright = P.getRight();
-        P.right = Q;
-        Q.left = Pright;
-        linkChild(Q, P);
-        return P;
-    }
+		if (t2 == null || t1 == null) {
+			return false;
+		} else
+			return t1.element.compareTo(t2.element)==0;
+	}
 
-    private Entry<T> left(Entry<T> P, Entry<T> Q) {
-        Entry<T> Qleft = Q.getLeft();
-        Q.left = P;
-        P.right = Qleft;
-        linkChild(P, Q);
-        return Q;
-    }
+	/**
+	 * Brings the node t to the root !
+	 * Bottom up splaying
+	 * @param t
+	 *            BST.Entry type, node to be brought to root
+	 */
+	private void splay(Entry<T> node) {
+		if(stack.isEmpty()||stack.peek()==null){
+			return;
+		}
+         
+		Entry<T> top = null;
+		Entry<T> parent = (stack.isEmpty() ? null : stack.pop());
+		Entry<T> grandparent = (stack.isEmpty() ? null : stack.pop());
+		
+		boolean pcl = compare(node, parent.getLeft());
+		boolean pcr = compare(node, parent.getRight());
+	    if (grandparent == null) {
+			if (pcl) {
+				top = rightRotate(parent);
+			}
+			if (pcr) {
+				top = leftRotate(parent);
+			}
+		}
+		else{
+			boolean gpr = compare(parent, grandparent.getRight());
+			boolean  gpl = compare(parent, grandparent.getLeft());
+		    if (pcl) {
+				if (gpl) {
+					Entry<T> res = rightRotate(grandparent);
+                    top = rightRotate(res);
+				}
+				if (gpr) {
+					grandparent.right = rightRotate(parent);
+					top = leftRotate(grandparent);
+				}
+			} else if (pcr) {
+				if (gpr) {
+					Entry<T> res = leftRotate(grandparent);
+                    top = leftRotate(res);
+				}
+				if (gpl) {
+					grandparent.left = leftRotate(parent);
+					top = rightRotate(grandparent);
+				}
+			}
 
-    private Entry<T> rightRight(Entry<T> P, Entry<T> Q, Entry<T> R) {
-        Entry<T> Pright = P.right;
-        Entry<T> Qright = Q.right;
-        Q.right = R;
-        R.left = Qright;
-        P.right = Q;
-        Q.left = Pright;
-        return P;
-    }
+		}
+		if (stack.isEmpty() || stack.peek() == null) {
+			root = top;
+			return;
 
-    private Entry<T> leftLeft(Entry<T> P, Entry<T> Q, Entry<T> R) {
-        Entry<T> Qleft = Q.getLeft();
-        Entry<T> Rleft = R.getLeft();
-        Q.left = P;
-        P.right = Qleft;
-        R.left = Q;
-        Q.right = Rleft;
-        return R;
-    }
+		} else {
+           splay(top);
+		}
 
-    private Entry<T> leftRight(Entry<T> P, Entry<T> R, Entry<T> Q) {
-        Entry<T> Qright = Q.getRight();
-        Entry<T> Qleft = Q.getLeft();
-        Q.left = R;
-        Q.right = P;
-        P.left = Qright;
-        R.right = Qleft;
-        return Q;
-    }
+	}
 
-    private Entry<T> rightLeft(Entry<T> P, Entry<T> R, Entry<T> Q) {
-        Entry<T> Qright = Q.getRight();
-        Entry<T> Qleft = Q.getLeft();
-        Q.right = R;
-        Q.left = P;
-        P.right = Qleft;
-        R.left = Qright;
-        return Q;
-    }
+	public static void main(String[] args) {
+		SplayTree<Integer> t = new SplayTree<>();
+		Scanner in = new Scanner(System.in);
+		while (in.hasNext()) {
+			int x = in.nextInt();
+			if (x > 0) {
+				System.out.print("Add " + x + " : ");
+				t.add(x);
+				
+				t.printTree();
+				
+			} else if (x < 0) {
+				System.out.print("Remove " + x + " : ");
+				t.remove(-x);
+				t.printTree();
+				
+			} else {
+				Comparable[] arr = t.toArray();
+				System.out.print("Final: ");
+				for (int i = 0; i < t.size; i++) {
+					System.out.print(arr[i] + " ");
+				}
+				System.out.println();
+				
+				return;
+			}
+		}
+	}
 
-    public static void main(String[] args) {
-        SplayTree<Integer> t = new SplayTree<>();
-        Scanner in = new Scanner(System.in);
-        while (in.hasNext()) {
-            int x = in.nextInt();
-            if (x > 0) {
-                System.out.print("Add " + x + " : ");
-                t.add(x);
-                System.out.println(t.isValid(x));
-                t.printTree();
-            } else if (x < 0) {
-                System.out.print("Remove " + x + " : ");
-                t.remove(-x);
-                t.printTree();
-            } else {
-                Comparable[] arr = t.toArray();
-                System.out.print("Final: ");
-                for (int i = 0; i < t.size; i++) {
-                    System.out.print(arr[i] + " ");
-                }
-                System.out.println();
-                return;
-            }
-        }
-    }
+	
 
-    /**
-     * CHECK VALIDITY
-     */
+	/**
+	 * CHECK VALIDITY
+	 */
 
-    private boolean isValid(T x) {
-        if(super.isValid(this.root))
-            return this.isValid(this.root, x);
-        return false;
-    }
+	private boolean isValid(T x) {
+		if (super.isValid(this.root))
+			return this.isValid(this.root, x);
+		return false;
+	}
 
-    private boolean isValid(Entry<T> node, T x) {
-        return node.element==x;
-    }
-
+	private boolean isValid(Entry<T> node, T x) {
+		return node.element == x;
+	}
 
 }
-
 
 /**
  * 10 20 30 40 50 60 70 1 2 15 16 35 45
